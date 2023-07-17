e4e65de0439e93c036d1c29092c1e039058d8c80
Anuradha Aggarwal <I1333@IT002600.local>
2023-07-17 13:51:29 +0530

    feat: add max-width to search header

--- src/Header/index.js
+++ src/Header/index.js
@@ -2,11 +2,12 @@ import React from "react";
 import { Button } from "@innovaccer/design-system";
 import { HeaderSearch } from "./HeaderSearch";
 import HeaderFilters from "./HeaderFilters";
+import '../style.css';
 
 export const Header = (props) => {
   return (
     <div>
-      <div className="d-flex mb-4 w-50">
+      <div className="d-flex mb-4 w-50 Header-wrapper">
         <HeaderSearch />
         <Button icon="add" onClick={props.toggleVerticalFilter}>
           More Filters
