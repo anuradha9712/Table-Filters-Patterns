dd9e81b42aaa1128027a19d17396a7b0fe67c6e4
anuradha9712 <anuradha.mcs18.du@gmail.com>
2023-10-05 17:00:02 +0400

    feat: add quick filters animation

--- src/Header/HeaderButton/index.js
+++ src/Header/HeaderButton/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import { Button, Dropdown } from "@innovaccer/design-system";
 
 export const HeaderButton = ({
@@ -8,88 +8,87 @@ export const HeaderButton = ({
   toggleVerticalFilter,
   showVerticalFilters,
 }) => {
-
-  const headerClass = showVerticalFilters ? "Header-filters--hide" : "Header-filters--show";
+  const headerClass = showVerticalFilters
+    ? "Header-filters--hide"
+    : "Header-filters--show";
 
   return (
-    <div className={headerClass}>
-      {/* {!showVerticalFilters && ( */}
-        <div className="d-flex align-items-center">
-          <Dropdown
-            className="ml-6"
-            disabled={loading}
-            withCheckbox={true}
-            showApplyButton={true}
-            inlineLabel="Name"
-            key={filterList["name"]}
-            onChange={(selected) => onFilterChange("name", selected)}
-            options={[
-              {
-                label: "A-G",
-                value: "a-g",
-                selected: filterList["name"]?.includes("a-g"),
-              },
-              {
-                label: "H-R",
-                value: "h-r",
-                selected: filterList["name"]?.includes("h-r"),
-              },
-              {
-                label: "S-Z",
-                value: "s-z",
-                selected: filterList["name"]?.includes("s-z"),
-              },
-            ]}
-          />
+    <div className={headerClass} id='header-quick-filters'>
+      <div className="d-flex align-items-center">
+        <Dropdown
+          className="ml-6"
+          disabled={loading}
+          withCheckbox={true}
+          showApplyButton={true}
+          inlineLabel="Name"
+          key={filterList["name"]}
+          onChange={(selected) => onFilterChange("name", selected)}
+          options={[
+            {
+              label: "A-G",
+              value: "a-g",
+              selected: filterList["name"]?.includes("a-g"),
+            },
+            {
+              label: "H-R",
+              value: "h-r",
+              selected: filterList["name"]?.includes("h-r"),
+            },
+            {
+              label: "S-Z",
+              value: "s-z",
+              selected: filterList["name"]?.includes("s-z"),
+            },
+          ]}
+        />
 
-          <Dropdown
-            className="ml-4"
-            disabled={loading}
-            withCheckbox={true}
-            showApplyButton={true}
-            inlineLabel="Gender"
-            key={filterList["gender"]}
-            onChange={(selected) => onFilterChange("gender", selected)}
-            options={[
-              {
-                label: "Male",
-                value: "male",
-                selected: filterList["gender"]?.includes("male"),
-              },
-              {
-                label: "Female",
-                value: "female",
-                selected: filterList["gender"]?.includes("female"),
-              },
-            ]}
-          />
+        <Dropdown
+          className="ml-4"
+          disabled={loading}
+          withCheckbox={true}
+          showApplyButton={true}
+          inlineLabel="Gender"
+          key={filterList["gender"]}
+          onChange={(selected) => onFilterChange("gender", selected)}
+          options={[
+            {
+              label: "Male",
+              value: "male",
+              selected: filterList["gender"]?.includes("male"),
+            },
+            {
+              label: "Female",
+              value: "female",
+              selected: filterList["gender"]?.includes("female"),
+            },
+          ]}
+        />
 
-          <Dropdown
-            className="ml-4"
-            disabled={loading}
-            withCheckbox={true}
-            showApplyButton={true}
-            inlineLabel="Type"
-            key={filterList["type"]}
-            onChange={(selected) => onFilterChange("type", selected)}
-            options={[
-              {
-                label: "Batch Execution",
-                value: "Batch Execution",
-                selected: filterList["type"]?.includes("Batch Execution"),
-              },
-              {
-                label: "Test Function",
-                value: "Test Function",
-                selected: filterList["type"]?.includes("Test Function"),
-              },
-            ]}
-          />
-          <Button icon="add" className="ml-4" onClick={toggleVerticalFilter}>
-            More Filters
-          </Button>
-        </div>
-      {/* )} */}
+        <Dropdown
+          className="ml-4"
+          disabled={loading}
+          withCheckbox={true}
+          showApplyButton={true}
+          inlineLabel="Type"
+          key={filterList["type"]}
+          onChange={(selected) => onFilterChange("type", selected)}
+          options={[
+            {
+              label: "Batch Execution",
+              value: "Batch Execution",
+              selected: filterList["type"]?.includes("Batch Execution"),
+            },
+            {
+              label: "Test Function",
+              value: "Test Function",
+              selected: filterList["type"]?.includes("Test Function"),
+            },
+          ]}
+        />
+        <Button icon="add" className="ml-4" onClick={toggleVerticalFilter}>
+          More Filters
+        </Button>
+      </div>
     </div>
   );
 };
--- src/Header/index.js
+++ src/Header/index.js
@@ -7,11 +7,45 @@ import { SavedFilterView } from "./SavedFilterView";
 import "../style.css";
 
 export const Header = (props) => {
+  const { showVerticalFilters } = props;
   const [savedFilterList, setSavedFilterList] = React.useState([]);
   const [openSidesheet, setOpenSidesheet] = React.useState(false);
+  const headerButtonWidth = document.getElementById(
+    "header-quick-filters"
+  )?.clientWidth;
+
+  const keyframe = `
+    @keyframes slidePanelRight {
+      from {
+        right: 284px;
+      }
+      to {
+        right: ${headerButtonWidth}px;
+      }
+    }
+
+    @keyframes slidePanelLeft {
+      from {
+        left: 284px;
+      }
+      to {
+        left: 0;
+      }
+    }
+`;
+
+  const animation = showVerticalFilters
+    ? "slidePanelLeft 160ms cubic-bezier(0.2, 0, 0.38, 0.9)"
+    : "slidePanelRight 120ms cubic-bezier(0.2, 0, 0.38, 0.9)";
+
+  const styles = {
+    animation,
+    animationFillMode: "forwards",
+  };
 
   return (
     <div>
+      <style>{keyframe}</style>
       <div className="d-flex mb-4 w-50 Header-wrapper">
         <HeaderSearch updateSearchTerm={props.updateSearchTerm} />
         <HeaderButton {...props} />
@@ -22,6 +56,7 @@ export const Header = (props) => {
               className="ml-4"
               onClick={() => setOpenSidesheet(true)}
               disabled={true}
+              style={styles}
             >
               Filter views
             </Button>
@@ -31,6 +66,7 @@ export const Header = (props) => {
             icon="list"
             className="ml-4"
             onClick={() => setOpenSidesheet(true)}
+            style={styles}
           >
             Filter views
           </Button>
